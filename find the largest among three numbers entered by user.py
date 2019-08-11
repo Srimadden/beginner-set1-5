103a756ce08e42821514bfae27f4cf3061aa8c0f
Srimadden <36351818+Srimadden@users.noreply.github.com>
2019-08-11 11:05:31 +0530

    Create find the largest among three numbers entered by user.py

---
+++ find the largest among three numbers entered by user.py
@@ -0,0 +1,9 @@
+a=input("enter the value of a: ")
+b=input("enter the value of b: ")
+c=input("enter the value of c: ")
+if((a>b)&(a>c)):
+    print("a is BIG",a)
+elif(b>c):
+    print("b is BIG",b)
+else:
+    print("c is BIG",c)
